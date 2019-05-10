e149798f9fb988d9a0216c2f628d80bb71448bdd
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-05-10 14:58:18 +0200

    task4

--- task4.py
+++ task4.py
@@ -20,11 +20,28 @@
 
 # Write your function here
 
-
+def mostOccurences(arr):
+    mostNumber = arr[0]
+    maxNumberOfOccurences = 1
+    currNumberOfOccurences = 0
+    currNumber = 0
+    i = 1
+    while i < len(arr):
+        currNumber = arr[i]
+        currNumberOfOccurences = 0
+        j = 0
+        while j < len(arr):
+            if(arr[j] == currNumber):
+                currNumberOfOccurences = currNumberOfOccurences + 1
+            j = j + 1
+        if currNumberOfOccurences > maxNumberOfOccurences:
+            maxNumberOfOccurences = currNumberOfOccurences
+            mostNumber = currNumber
+        i = i + 1
+    return mostNumber
 
 def main():
-    # Test your function here
-    pass
-
-if __name__ == "__main__":
+    arr = [1, 3, 4, 6, 1, 1, 12, 12, 12, 12, 12, 12, 12]
+    mostNumber = mostOccurences(arr)
+    print("Number with most occurences: ", mostNumber)
 main()

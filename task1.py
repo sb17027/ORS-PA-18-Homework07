88ac31457996f8ff03d374ba5a359e2c6a0208e4
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-05-10 14:56:05 +0200

    task1

--- task1.py
+++ task1.py
@@ -17,3 +17,36 @@
 """
 
 # Write your script here
+from random import randint
+
+def main():
+    n = int(input("Insert number of times for rolling the dice: "))
+    i = 0
+    numberOfOnes = 0
+    numberOfTwos = 0
+    numberOfThrees = 0
+    numberOfFours = 0
+    numberOfFives = 0
+    numberOfSixes = 0
+    while i <= n:
+        num = randint(1, 6)
+        if num == 1:
+            numberOfOnes = numberOfOnes + 1
+        if num == 2:
+            numberOfTwos = numberOfTwos + 1
+        if num == 3:
+            numberOfThrees = numberOfThrees + 1
+        if num == 4:
+            numberOfFours = numberOfFours + 1
+        if num == 5:
+            numberOfFives = numberOfFives + 1
+        if num == 6:
+            numberOfSixes = numberOfSixes + 1
+        i = i + 1
+    print("Number of ones: ", numberOfOnes)
+    print("Number of twos: ", numberOfTwos)
+    print("Number of threes: ", numberOfThrees)
+    print("Number of fours: ", numberOfFours)
+    print("Number of fives: ", numberOfFives)
+    print("Number of sixes: ", numberOfSixes)
+main()
